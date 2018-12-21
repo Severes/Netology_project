60a2ee4f851607d6251eeb4083322ce8f621f41d
Severes <v.simigin@gmail.com>
2018-12-21 23:42:24 +0300

    formatting file to PEP8

--- HW_1.8/hw_1.7.py
+++ HW_1.8/hw_1.7.py
@@ -6,12 +6,14 @@ class Animals:
         self.color = color
         self.wool = wool
 
+
 class Cow(Animals):
     wheight = None
 
     def __init__(self, color, wool, wheight):
         self.weight = wheight
         super().__init__(color, wool)
+
 
 cow_1 = Cow("white", "Да", 1000)
 print(cow_1.__dict__)
