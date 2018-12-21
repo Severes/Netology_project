b8b2b3cf5772de4d4bcba2c91551ee76362b017c
Severes <v.simigin@gmail.com>
2018-12-21 23:40:35 +0300

    adding hw_1.7

---
+++ HW_1.8/hw_1.7.py
@@ -0,0 +1,17 @@
+class Animals:
+    color = None
+    wool = None
+
+    def __init__(self, color, wool):
+        self.color = color
+        self.wool = wool
+
+class Cow(Animals):
+    wheight = None
+
+    def __init__(self, color, wool, wheight):
+        self.weight = wheight
+        super().__init__(color, wool)
+
+cow_1 = Cow("white", "Да", 1000)
+print(cow_1.__dict__)
