5a26b8f18fc763cf359b1ae7386d3c742fb7940f
Severes <v.simigin@gmail.com>
2019-01-18 15:13:42 +0300

    ДЗ в процессе выполнения.

--- HW_2.2/HW_2.2.py
+++ HW_2.2/HW_2.2.py
@@ -13,6 +13,8 @@
 
 import chardet
 
+# открываем файл и считываем весь текст построчно, попутно вычисляя кодировку и форматируя итоговый список
+# таким образом, чтобы отобрать слова с длинной от 6 символов
 with open('newsafr.txt', 'rb') as text:
     six_digit_list = list()
     text2 = text.read()
@@ -26,7 +28,34 @@ with open('newsafr.txt', 'rb') as text:
             six_digit_list.append(word)
 print('Слова с более чем 6 символами:', six_digit_list)
 
-# доделать в части отбора только 10 слов, встречающихся чаще всего в каждом файле
+# считаем повторения слов в списке и создаем список из значения и его количества в списке
+six_digit_list_count = list()
+for item in six_digit_list:
+    x = item, six_digit_list.count(item)
+    if x not in six_digit_list_count:
+        six_digit_list_count.append(x)
+# print(six_digit_list_count)
+
+
+# функция, которая поможет отсортировать значения в списке в зависимости от количества повторений слов в списке
+def sorted_key(key):
+    return key[1]
+
+
+# сортируем список по количеству повторений значений этого списка
+sorted_six_digit_list_count = sorted(six_digit_list_count, key=sorted_key, reverse=True)
+
+# print('Отсортированный список', sorted_six_digit_list_count)
+
+hateful_10 = list()
+for a in range(10):
+    hateful_10.append(sorted_six_digit_list_count[a])
+    for b in hateful_10:
+
+print('10 максимально повторяющихся слов из списка: ', hateful_10)
+
+
+
 
 
 
